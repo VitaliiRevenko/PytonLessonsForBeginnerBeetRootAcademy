8892ab0ae87dd854b3df86b8ff4a3ca7f70b9499
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-03-25 01:03:49 +0200

    Add files via upload

--- lesson_6/advtask.py
+++ lesson_6/advtask.py
@@ -3,16 +3,73 @@
 В одну строку (ну или как обычно) создать словарь вида
 {"Понедельник" : 1, ...
 Так же в одну строку или как получится создать обратный словарь
-{ 1: "Понедельник",'''
-
-#print(week)
-def DictRevers(items):
-    new_dict = {}
-    for key, value in items:
-        key, value = value, key
-        new_dict.setdefault(key, value)
-    return new_dict
+{ 1: "Понедельник",
+'''
+'''
 a = {'Понедельник': 1, 'Вторник': 2, 'Среда': 3, 'Четверг': 4, 'Пятница': 5, 'Суббота': 6, 'Воскресенье': 7}
-b = DictRevers(a.items())
+b = dict([(i, j) for (j, i) in a.items()])
 print(f'Original dict\t{a}\n Revers dict\t{b}')
+'''
 
+##################
+'''1. программа играет с человеком угадывая загаданное число.
+ но не методом урезания промежутка пополам, а выбирая рандомное число из промежутка.
+import random
+human = int(input('Vvedite celoe chislo \t'))
+while True:
+    r = random.randint(0, human)
+    if r == human:
+        print(f'Your number:\t{r}')
+        break
+    else:
+        pass
+'''
+##################
+'''2. пока человек не введет q или Q или Й или й играем с ним в камень ножницы бумага.
+ сделайте множество или кортеж для набора вариантов. Рандомно выбирайте один из элементов листа (random.choice).
+2.1 для игры допишите учет статистики в словарь
+stats = {
+"K": 0, "N": 0, "B":0 }
+stats["K"] увеличиваем если человек загадал камень и так далее.
+подумайте и допишите если есть больше 10 игр учет статистики предпочтений в качестве веса для рандома.
+ (не случайно выбирать ПОТОМ фигуру а с учетом статистики. Делать рандом с учетом веса это пока рано но можете попробовать.
+  Пока циклы условия, списки, вхождение в кортеж, редактирование словарика статистики )'''
+import random
+stone, scissors, paper = 1, 2, 3
+
+win = (paper, stone), (stone, scissors), (scissors, paper)
+lose = (stone, paper), (scissors, stone), (paper, scissors)
+draw = (stone, stone), (scissors, scissors), (paper, paper)
+while True:
+    stats = {'stone': 0, 'scissors': 0, 'paper': 0}
+    player = int(input(f'1 - Stone\n2 - Scissors\n3 - Paper\nEner the number:\t'))
+    comp = random.randint(1, 3)
+    if player == 1:
+        stats['stone'] += 1
+    elif player == 2:
+        stats['scissors'] += 1
+    elif player == 3:
+        stats['paper'] += 1
+    stats.update()
+    comb = (player, comp)
+    print(f'player:\t{player}\tcomp:\t{comp}')
+    for i in win:
+        if comb == i:
+            print('You WIN')
+    for j in lose:
+        if comb == j:
+            print('You LOSE')
+        else:
+            continue
+    for x in draw:
+        if comb == x:
+            print('DRAW')
+        else:
+            continue
+    finish = input("For exit push 'q'\t")
+    if finish in {'q', 'Q'}:
+        print(comb)
+        break
+    elif finish in {'й', 'Й'}:
+        print(stats)
+        break
--- lesson_6/task3.py
+++ lesson_6/task3.py
@@ -3,10 +3,12 @@ List comprehension exercise
 Use a list comprehension to make a list containing tuples (i, j)
 where `i` goes from 1 to 10 and `j` is corresponding to `i` squared.'''
 
-i, j = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], []
+'''i, j = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], []
 index = 0
 while index <= 9:
-    j.append(i[index] * i[index])
+    j.append(i[index] ** 2)
     index += 1
-print(f'{i}\n{j}')
+print(f'{i}\n{j}')'''
 
+squares = [x**2 for x in range(10)]
+print(squares)
