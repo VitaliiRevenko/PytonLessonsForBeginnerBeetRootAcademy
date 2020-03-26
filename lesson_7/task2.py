7d85f6f79832a2b1e958c1a8cea69f5cee880471
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-03-26 23:14:40 +0200

    Update task2.py

--- lesson_7/task2.py
+++ lesson_7/task2.py
@@ -4,6 +4,18 @@ Create a function called make_country, which takes in a country’s name and cap
 Then create a dictionary from those parameters, with ‘name’ and ‘capital’ as keys.
 Make the function print out the values of the dictionary to make sure that it works as intended.'''
 
-def make_country(**kvargs):
-    print(f'{len(kvargs)}\tArguments\nName of country and capital:\t, {kvargs}')
-make_country(name = 'Ukraine', capital = 'Kyiv')
+#def make_country(**kvargs):
+#    print(f'{len(kvargs)}\tArguments\nName of country and capital:\t, {kvargs}')
+#make_country(name = 'Ukraine', capital = 'Kyiv')
+
+# Переписал задание ниже
+
+def make_country(name, capital, **kwargs):  # Функция получает параметры и записывает в словарь
+   kwargs = {'name': name, 'capital': capital}
+   return(kwargs)                           # Возвращаем функции словарь
+def print_dict(name_capital):               # Функция выводит на печать словарь
+    print(name_capital)
+name = 'Canada'
+capital = 'Ottawa'
+name_capital = make_country(name, capital)  # В Переменную записываем результат функции
+print_dict(name_capital)                    # Вызов функции печати словаря
