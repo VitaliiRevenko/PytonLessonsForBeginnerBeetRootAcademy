8aed09cc6a69c9676b6439c4990f948d9721928d
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-03-27 19:48:49 +0200

    Add files via upload

---
+++ lesson_8/task1.py
@@ -0,0 +1,18 @@
+'''Task 1
+Files
+Write a script that creates a new output file called myfile.txt and writes the string
+"Hello file world!" in it. Then write another script that opens myfile.txt, and reads and prints its contents.
+ Run your two scripts from the system command line.
+Does the new file show up in the directory where you ran your scripts?
+What if you add a different directory path to the filename passed to open?
+Note: file write methods do not add newline characters to your strings; add an explicit
+‘\n’ at the end of the string if you want to fully terminate the line in the file.'''
+# Записываем в файл
+hello = 'Hello file world'
+with open("myfile.txt", 'w') as file_object:
+    file_object.write(hello)
+
+# Выводим на печать из файла
+with open('myfile.txt') as file_object:
+    hello2 = file_object.read()
+print("This are data from myfile:\t" + hello2, "!!!")
