735af161bdf25a5bc417e21925e6daffaa703c41
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-04-08 15:48:21 +0300

    Add files via upload

---
+++ lesson_11/task1.py
@@ -0,0 +1,44 @@
+'''Task 1
+School
+Make a class structure in python representing people at school. Make a base class called Person, a class called Student,
+and another one called Teacher. Try to find as many methods and attributes as you can which belong to different classes,
+and keep in mind which are common and which are not. For example, the name should be a Person attribute,
+while salary should only be available to the teacher. '''
+'''Школа Создайте структуру классов в Python, представляющую людей в школе. Создайте базовый класс с именем Person,
+ класс с именем Student и еще один с именем Teacher. Постарайтесь найти как можно больше методов и атрибутов,
+  которые принадлежат разным классам, и имейте в виду, какие распространены, а какие нет. Например,
+   имя должно быть атрибутом Person, а зарплата должна быть доступна только учителю.'''
+
+class Person:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+    def printname(self):
+        print(self.fname, self.lname)
+    pass
+class Student(Person):
+    def __init__(self, fname, lname, group, course, faculty, form_of_education):
+        super().__init__(fname, lname)
+        self.group = group
+        self.course = course
+        self.faculty = faculty
+        self.form_of_education = form_of_education
+    def printname1(self):
+        print(self.group, self.course, self.faculty, self.form_of_education)
+class Teacher(Person):
+    def __init__(self, fname, lname, object_of_study, salary):
+        super().__init__(fname, lname)
+        self.object_of_study = object_of_study
+        self.salary = salary
+    def printname2(self):
+            print(self.object_of_study, self.salary)
+if __name__ == "__main__":
+    student1 = Student("Revenko", "Vitalii", "201", "2", "IT", "budget")
+    #print(f"ФИО студента:\t{student1.printname()}, {student1.printname1()}")
+    student1.printname()
+    student1.printname1()
+    print("-=-"*10)
+    teacher = Teacher("Shevchenko", "Taras", "Literatura", "1000")
+    teacher.printname()
+    teacher.printname2()
+
