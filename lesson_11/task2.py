de7dfaabdb22ec85ec7695b657377c292d232c02
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-04-10 10:37:38 +0300

    Update task2.py

--- lesson_11/task2.py
+++ lesson_11/task2.py
@@ -18,6 +18,10 @@ filter_leaps (берет список дат (целых) и удаляет те
         assert m.square_nums([7, 11, 5, 4]) == [49, 121, 25, 16]
         assert m.remove_positives([26, -11, -8, 13, -90]) == [-11, -8, -90]
         assert m.filter_leaps([2001, 1884, 1995, 2003, 2020]) == [1884, 2020]'''
+from datetime import date
+today = date.today()
+#print(today.year)
+#print("Today's date:", today)
 class Mathematician:
     def square_nums(self, *args):
         print(f"Incoming data:\n", args)
@@ -34,8 +38,27 @@ class Mathematician:
     def filter_leaps(self,*args):
         print(f"\nIncoming data:\n", args)
         print("Filter leaps:")
-
+        #leap_year = args[0]
+        for i in args:
+            '''Определить високосный год или нет
+            Если год не делится на 4, значит он обычный.
+            Иначе надо проверить не делится ли год на 100.
+            Если не делится, значит это не столетие и можно сделать вывод, что год високосный.
+            Если делится на 100, значит это столетие и его следует проверить его делимость на 400.
+            Если год делится на 400, то он високосный.'''
+            # Если остаток от деления на 4 не равен нулю,
+            # значит год не делится нацело на 4 и
+            # не является високосным, т. е. он обычный.
+            # Исключаем столетия, которые не делятся на 400
+            if i % 4 != 0 or (i % 100 == 0 and i % 400 != 0):
+                pass
+                print("usual year:\t", i)
+            else:
+                print("Leap year:\t", i)
 m = Mathematician()
 m.square_nums(7, 11, 5, 4)
 m.remove_positives(26, -11, -8, 13, -90)
-m.filter_leaps(2001, 1884, 1995, 2003, 2020)
+m.filter_leaps(2001, 1884, 1995, 2003, today.year)
+
+
+
