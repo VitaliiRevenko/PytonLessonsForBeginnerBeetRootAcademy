8c50f0e44a355262be66efab19e4fe88d87fdd76
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-04-15 17:30:40 +0300

    Update task2.py

--- lesson_11/task2.py
+++ lesson_11/task2.py
@@ -57,7 +57,7 @@ class Mathematician:
         print(f"Filter leaps:{new_list}")
         return new_list
 if __name__ == '__main__':
-        m = Mathematician()
-        assert m.square_nums([7, 11, 5, 4]) == [49, 121, 25, 16]
-        assert m.remove_positives([26, -11, -8, 13, -90]) == [-11, -8, -90]
-        assert m.filter_leaps([2001, 1884, 1995, 2003, 2020]) == [1884, 2020]
+    m = Mathematician()
+    assert m.square_nums([7, 11, 5, 4]) == [49, 121, 25, 16]
+    assert m.remove_positives([26, -11, -8, 13, -90]) == [-11, -8, -90]
+    assert m.filter_leaps([2001, 1884, 1995, 2003, 2020]) == [1884, 2020]
