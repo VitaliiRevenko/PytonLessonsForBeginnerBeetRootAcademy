cecf092972c0b39fdd5a9688c751d9b27c75121f
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-04-18 09:19:59 +0300

    Add files via upload

---
+++ lesson_13/task2.py
@@ -0,0 +1,12 @@
+# Task 2
+# Write a Python program to access a function inside a function (Tips: use function, which returns another function)
+def test(word):
+    def low(it):
+        if it.isdigit():
+            return 'N'
+        return it.lower()
+    res = ''
+    for i in word:
+        res += low(i)
+    return res
+print(test('BEET Root Academy'))
