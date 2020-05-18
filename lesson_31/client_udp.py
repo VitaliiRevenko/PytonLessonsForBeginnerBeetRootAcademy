c0ed1dee3e5bb1671c3217545453465f338f550e
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-05-18 16:01:42 +0300

    Add files via upload

--- lesson_31/client_udp.py
+++ lesson_31/client_udp.py
@@ -1,11 +1,15 @@
 import socket
 
-HOST = '127.0.0.1'  # localchost
-PORT = 5005    # Port to listen on
+msgFromClient = "Hi UDP Server"
+bytesToSend = str.encode(msgFromClient)
+serverAddressPort = ("127.0.0.1", 5000)
+bufferSize = 1024
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
-    udp_socket.connect((HOST, PORT)) # conect
-    udp_socket.sendall(b'Hello, world')  # send all data
-    data = udp_socket.recv(1024)
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-print("Recevied", repr(data))
+# Send to server using created UDP socket
+UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+msg = "Message from Server {}".format(msgFromServer[0])
+print(msg)
--- lesson_31/server_udp.py
+++ lesson_31/server_udp.py
@@ -1,29 +1,37 @@
 import socket
 
-#Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+import argparse
+parser = argparse.ArgumentParser(description="Server UDP")
+parser.add_argument('--host', '-i', default='127.0.0.1', required=False, help='Which IP adress')
+parser.add_argument('--port', '-p', default='5000', type=int, required=False, help='Which port')
+parser.add_argument('--buffer_size', '-b', default='1024', type=int, required=False, help='Buffer size')
+args = parser.parse_args()
+print(args)
 
-# Bind the socket to the port
-server_address = ('localhost', 5005)
-print("starting up on {} port {}".format(*server_address))
+localIP = "127.0.0.1"
+localPort = 5000
+bufferSize = 1024
 
-# Listen for incoming connection
-sock.listen(1)
+msgFromServer = "Hi UDP Client"
+bytesToSend = str.encode(msgFromServer)
 
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-    try:
-        print('connection from', client_address)
-        while True:
-            data = connection.recv(1024)  # read data from socket
-            print('recevied{!r}'.format(data))
-            if data:
-                print('sending data back to the client')
-                connection.sendall()
-            else:
-                print('no data from', client_address)
-                break
-    finally:
-        connection.close()
+# Create a datagram socket
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Bind to address and ip
+UDPServerSocket.bind((localIP, localPort))
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+
+while (True):
+    data = UDPServerSocket.recvfrom(bufferSize) # read data from socket
+    message = data[0]
+    address = data[1]
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP = "Client IP Address:{}".format(address)
+    print(clientMsg)
+    print(clientIP)
+
+    # Sending a reply to client
+    UDPServerSocket.sendto(bytesToSend, address)
