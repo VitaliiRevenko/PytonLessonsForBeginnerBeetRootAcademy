97f2723304bd99630e295a6a543955039b1d3cff
VitaliiRevenko <62260768+VitaliiRevenko@users.noreply.github.com>
2020-05-18 17:20:46 +0300

    Add files via upload

--- lesson_31/server_udp2.py
+++ lesson_31/server_udp2.py
@@ -1,32 +1,29 @@
 import socket
-
 import argparse
 
 if __name__ == '__main__':
 
+    localIP = "127.0.0.1"
+    localPort = 5000
+    bufferSize = 1024
+    global conf, bytesToSend
+    conf = [localIP, localPort, bufferSize]
+    msgFromServer = "Hi UDP Client"
+    bytesToSend = str.encode(msgFromServer)
+
+
     def load_conf():    # Function for load server configuration
         parser = argparse.ArgumentParser(description="Server UDP")
-        parser.add_argument('--host', '-i', default='127.0.0.1', required=False, help='Which IP adress')
-        parser.add_argument('--port', '-p', default='5000', type=int, required=False, help='Which port')
-        parser.add_argument('--buffer_size', '-b', default='1024', type=int, required=False, help='Buffer size')
+        parser.add_argument('--host', '-i', default=conf[0], required=False, help='Which IP adress')
+        parser.add_argument('--port', '-p', default=conf[1], type=int, required=False, help='Which port')
+        parser.add_argument('--buffer_size', '-b', default=conf[2], type=int, required=False, help='Buffer size')
         args = parser.parse_args()
         print(args)
-        localIP = "127.0.0.1"
-        localPort = 5000
-        global bufferSize
-        bufferSize = 1024
-
-        msgFromServer = "Hi UDP Client"
-        global bytesToSend
-        bytesToSend = str.encode(msgFromServer)
-
-        # Create a datagram socket
         global UDPServerSocket
         UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
         # Bind to address and ip
         UDPServerSocket.bind((localIP, localPort))
-        print("UDP server up and listening")
 
     def start_server(): # Function for start server
         data = UDPServerSocket.recvfrom(bufferSize)  # read data from socket
@@ -41,6 +38,7 @@ if __name__ == '__main__':
         UDPServerSocket.sendto(bytesToSend, address)
 
     load_conf()
+    print("UDP server up and listening")
     try:
         # Listen for incoming datagrams
         while (True):
